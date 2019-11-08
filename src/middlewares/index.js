1a1764bd10f99e145ce9ddb163f11012a513da8c
ohager <oliver.hager@dextra-sw.com>
2019-11-08 18:18:04 -0300

    rename middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,14 +2,14 @@ import compression from 'compression'
 import sirv from 'sirv'
 import { middleware as sapper } from '@sapper/server'
 import { json } from 'body-parser'
-import { serveBff } from './serveBff'
+import { serveApi } from './serveApi'
 import { logRequests } from './logRequests'
 
 const dev = process.env.NODE_ENV === 'development'
 
 export const middlewares = [
     compression({ threshold: 0 }),
-    serveBff(),
+    serveApi(),
     json(),
     sirv('static', { dev }),
     logRequests(),
