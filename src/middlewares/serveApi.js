915b72bad0bf7de8a0d1d7669343e8bfddcc7847
ohager <oliver.hager@dextra-sw.com>
2019-11-08 18:18:04 -0300

    rename middleware

--- src/middlewares/serveApi.js
+++ src/middlewares/serveApi.js
@@ -1,6 +1,6 @@
 const isApiCall = req => req.path.toLowerCase().startsWith('api')
 
-export const serveBff = () => (req, res, next) => {
+export const serveApi = () => (req, res, next) => {
     if (isApiCall(req)) {
         res.setHeader('Content-Type', 'application/json')
     }
