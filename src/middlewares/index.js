87081562e6d9b6bb6f533e0c5ac9209fd427351e
ohager <oliver.hager@dextra-sw.com>
2019-11-12 21:55:23 -0300

    Adding CORS support

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,12 +2,14 @@ import compression from 'compression'
 import sirv from 'sirv'
 import { middleware as sapper } from '@sapper/server'
 import { json } from 'body-parser'
+import cors from 'cors'
 import { serveApi } from './serveApi'
 import { logRequests } from './logRequests'
 
 const dev = process.env.NODE_ENV === 'development'
 
 export const middlewares = [
+    cors(),
     compression({ threshold: 0 }),
     serveApi(),
     json(),
