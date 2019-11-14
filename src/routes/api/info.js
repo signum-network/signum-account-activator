0f58894cdb014b5c620765b132c48bf335a9f5cd
ohager <oliver.hager@dextra-sw.com>
2019-11-14 10:59:22 -0300

    Added OpenAPI descriptor

---
+++ src/routes/api/info.js
@@ -0,0 +1,10 @@
+import apiDescription from './__content__/description.openapi'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+
+export const get = async (req, res) => {
+    apiDescription.host = req.headers.host
+
+    res.end(JSON.stringify(apiDescription))
+}
