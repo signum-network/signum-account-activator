12f472d02775fe10ec176ee935f20f62f8e780e8
ohager <oliver.hager@dextra-sw.com>
2020-08-01 15:03:27 -0300

    fix: removed invalid host prop of openapi desc

--- src/routes/api/info.js
+++ src/routes/api/info.js
@@ -2,8 +2,6 @@ import apiDescription from './__content__/description.openapi'
 
 const isDev = process.env.NODE_ENV === 'development'
 
-
 export const get = async (req, res) => {
-    apiDescription.host = req.headers.host
     res.end(JSON.stringify(apiDescription))
 }
