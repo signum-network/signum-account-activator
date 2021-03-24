c19beebf47b70c77c089a3876723c4180e4af09e
ohager <oliver.hager@dextra-sw.com>
2021-03-23 21:30:00 -0300

    fix: accept real numbers as initial amount

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ export const config = {
     burstNodeHost: process.env.BURST_NODE,
     donationAccount: process.env.DONATION_ACCOUNT,
     accountSecret: process.env.ACTIVATOR_ACCOUNT_SECRET,
-    activationAmount: parseInt(process.env.ACTIVATION_AMOUNT, 10),
+    activationAmount: parseFloat(process.env.ACTIVATION_AMOUNT),
     isTestnet: isTrue(process.env.TEST_NET),
     webUiAvailable: isTrue(process.env.WEB_UI),
 }
