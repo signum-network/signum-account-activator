140f6bd0ab096aee5e694efc7e2b69bac367349c
ohager <oliver@devbutze.com>
2023-05-16 12:06:59 -0300

    fix: testnet

--- src/routes/api/__services__/ActivatorService.js
+++ src/routes/api/__services__/ActivatorService.js
@@ -85,6 +85,7 @@ export class ActivatorService {
     async __sendWelcomeMessage(accountId, publicKey) {
         const { signPrivateKey, publicKey: senderPublicKey } = this.__getSenderCredentials()
         const feePlanck = Amount.fromSigna(config.activationFee).getPlanck()
+        // FIXME: once 3.7 is live
         const sendMessageArgs = {
             message: publicKey,
             // message: WelcomeMessage,
